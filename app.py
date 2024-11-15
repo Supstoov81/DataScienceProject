cf2cd8ba73130d922868cba72707f6620281dcf4
NGOM-cheikh <mourtalla211@gmail.com>
2024-11-15 21:21:32 +0100

    Message décrivant les modifications apportées

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+
 import streamlit as st
 from sections.classification.classification import classification_page
 from sections.nailsdetection.nails import nail_page
 from sections.regression.regression import regression_page
 
+# La première commande Streamlit dans le fichier
 st.set_page_config(
     page_title="Playground ML",
     page_icon="🧊",
@@ -10,11 +12,13 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+# Choix dans la barre latérale
 type_data = st.sidebar.radio(
     "Choisissez votre type de playground",
     ["Regression", "Classification", "NailsDetection"]
 )
 
+# Chargement des pages en fonction du choix
 if type_data == "Regression":
     regression_page()
 elif type_data == "Classification":
